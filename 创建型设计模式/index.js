ce3e67959bf62c531a0e08ae8f52e4cce3484ef7
wangziyi0526 <568771088@qq.com>
2020-04-26 20:02:32 +0800

    创建型设计模式

---
+++ 创建型设计模式/index.js
@@ -0,0 +1,47 @@
+// - 简单工厂模式，又叫静态工厂模式，由一个工厂对象决定创建某一种产品对象类的实例，主要用来创建同一类对象
+// 第一个类 篮球
+var Basketball = function (){
+  this.intro = "篮球盛行于美国"
+};
+Basketball.prototype = {
+  getMember: function(){
+    console.log('每个队伍需要5名球员')
+  },
+  getBallSize: function(){
+    console.log('篮球是圆的')
+  }
+};
+
+var FootBall =  function(){
+  this.intro = '中国足球，世界驰名';
+};
+// 第二个类  足球
+FootBall.prototype = {
+  getMember: function(){
+    console.log('中国足球，远近闻名')
+  },
+  getBallSize: function(){
+    console.log('足球是踢的')
+  }
+};
+
+// 工厂模式
+
+var sportsFactroy = function(name){
+  switch(name){
+    case 'NBA':
+      return new Basketball();
+    case 'worldCup':
+      return new FootBall();
+  }
+}
+
+var football = sportsFactroy('worldCup');
+console.log(football);
+console.log(football.intro);
+football.getMember();
+
+var basketball = sportsFactroy('NBA');
+console.log(basketball);
+basketball.getMember();
+
