b5d57282503997821427d20f7043708899082e47
王子一 <wangzy@nancal.com>
2022-04-27 13:17:04 +0800

    抽象工厂函数工具类

--- 工厂模式/index.js
+++ 工厂模式/index.js
@@ -2,20 +2,57 @@
  * @Author: wangzy
  * @Date: 2022-04-24 13:49:28
  * @LastEditors: wangzy
- * @LastEditTime: 2022-04-26 14:09:52
- * @Description: 工厂模式 构造函数
+ * @LastEditTime: 2022-04-27 13:16:41
+ * @Description: 工厂模式 构造函数 工具类
  */
 
-class MobileFactory {
-  // 提供操作系统的接口
-  createOS() {
+// 抽象工具工厂
+class Utils {
+  // 操作Object
+  operationObject() {
     throw new Error('抽象工厂方法不允许直接调用，你需要将我重写!')
   }
-  // 提供硬件的接口
-  createHardWare() {
+  // 操作Array
+  operationArray() {
+    throw new Error('抽象工厂方法不允许直接调用，你需要将我重写!')
+  }
+  // 正则校验
+  operationRegExp() {
+    throw new Error('抽象工厂方法不允许直接调用，你需要将我重写!')
+  }
+  // 操作String
+  operationOString() {
+    throw new Error('抽象工厂方法不允许直接调用，你需要将我重写!')
+  }
+  // 操作Number
+  operationNumber() {
     throw new Error('抽象工厂方法不允许直接调用，你需要将我重写!')
   }
 }
 
-let my = new MobileFactory()
-my.createHardWare()
+// 具体工具工厂
+
+class myUtils extends Utils {
+  operationObject() {
+    return new Obj()
+  }
+}
+
+// Obj 工具库
+
+class Obj {
+  // 合并对象
+  assginObj() {
+    let data = {}
+    for (let i = 0; i < arguments.length; i++) {
+      Object.assign(data, arguments[i])
+    }
+    return data
+  }
+  // 根据key 查找对象中是否存在的键值对，有则返回value 无则返回fasle
+}
+
+let my_utils = new myUtils()
+
+let test = my_utils.operationObject().assginObj(data, data1)
+console.log(test)
