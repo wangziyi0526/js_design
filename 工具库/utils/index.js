d1361c437c3f855b55b962d08dc50f85b33432eb
王子一 <568771088@qq.com>
2022-06-18 22:26:47 +0800

    防抖函数

---
+++ 工具库/utils/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: wangzy
+ * @Date: 2022-06-15 11:36:52
+ * @LastEditors: wangzy
+ * @LastEditTime: 2022-06-15 12:27:06
+ * @Description: 日常积累 工具库
+ */
+import { checkIsNull } from './utils/object.js'
+let data = {
+  name: 'wzy',
+}
+checkIsNull(data)
--- 防抖函数/index.js
+++ 防抖函数/index.js
@@ -2,14 +2,14 @@
  * @Author: wangzy
  * @Date: 2022-06-15 10:31:13
  * @LastEditors: wangzy
- * @LastEditTime: 2022-06-15 11:25:03
+ * @LastEditTime: 2022-06-18 22:23:38
  * @Description: 原生js 手写 防抖函数 利用定时器
  */
 
 let btn = document.getElementById('btn')
 btn.addEventListener('click', debounce(getValue, 2000))
 function getValue() {
-  console.log(123)
+  console.log(111)
 }
 function debounce(fn, time) {
   let t = null
@@ -17,9 +17,9 @@ function debounce(fn, time) {
     var fristHandle = !t
     if (fristHandle) {
       fn()
+      t = setTimeout(function () {
+        t = null
+      }, time)
     }
-    t = setTimeout(function () {
-      t = null
-    }, time)
   }
 }
